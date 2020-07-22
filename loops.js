eef525307669c1a3482eec9baca32964a4be09e8
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-07-22 14:19:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,28 +1,32 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop")
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
-    }
-    return array
-  }
+	for (let i = 0; i < 25; i++) {
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
+		}
+	}
 
-function whileLoop(number) {
-  while (number > 0){
-    console.log(number--)
-  }
-  return "done"
+	return array;
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
+
+	return 'done';
 }
 
-function doWhileLoop(array) {
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
   do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
-  return array
-}
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+} 
